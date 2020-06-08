c9fc55c1e1f9b773c4d07c86a090bb19c34878d1
raniasalem9998 <rania.salem9998@gmail.com>
2020-06-08 17:55:37 +0300

    test

--- js/app.js
+++ js/app.js
@@ -3,44 +3,44 @@
 var name = prompt("WHAT IS YOUR NAME?",'visitor');
 alert ("hello"+ name);
 var cat.toLowerCase() = prompt("Do I like cats?");
-var tall = prompt("Am I not 150cm tall?");
-var sleep = prompt("Do I have control on my sleep scedule?");
-var plants = prompt("Do I like plants?");
-var physics = prompt("Am I good at physics?");
+var tall.toLowerCase()  = prompt("Am I not 150cm tall?");
+var sleep.toLowerCase()  = prompt("Do I have control on my sleep scedule?");
+var plants.toLowerCase()  = prompt("Do I like plants?");
+var physics.toLowerCase()  = prompt("Am I good at physics?");
 
-if (cat == "yes") {
+if (cat.toLowerCase() == "yes") {
     alert("sure I do!");
 }
  else {
-     alert("wrong");
+     alert("1- wrong");
 }
 
 if (tall.toLowerCase() == "yes") {
     alert("proudely yes!");
 }
 else { 
-    alert("wrong");
+    alert("2- wrong");
 }
 
 if (sleep.toLowerCase() == "no") {
     alert("sadely"); 
 }
 else {
-    alert("wrong");
+    alert("3- wrong");
 }
 
 if (plants.toLowerCase() == "no"){
     alert("correct");
 }
 else {
-    alert("wrong");
+    alert("4- wrong");
 }
 if (physics.toLowerCase()=="yes"){
     alert ("correct");
     
 }
 else {
-    alert ("wrong");
+    alert ("5- wrong");
 }
 
 alert ("Bye"+ name);
