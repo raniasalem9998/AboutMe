c1b663eb8349d41ff251abe81758dbcfccc866fe
raniasalem9998 <rania.salem9998@gmail.com>
2020-06-10 11:51:16 +0300

    for loop

--- js/app.js
+++ js/app.js
@@ -1,80 +1,82 @@
 'use strict'
-
-var name = prompt("WHAT IS YOUR NAME?", 'visitor');
-alert("hello" + name);
 var correct = 0;
-var cat = prompt("Do I like cats?");
-if (cat.toLowerCase() == "yes") {
-    alert("sure I do!");
-    var correct = correct + 1;
-}
-else {
-    alert("1- wrong");
+// var name = prompt("WHAT IS YOUR NAME?", 'visitor');
+// alert("hello" + name);
 
-}
+// var cat = prompt("Do I like cats?");
+// if (cat.toLowerCase() == "yes" || cat.toLowerCase() == "y") {
+//     alert("sure I do!");
+//     var correct = correct + 1;
+// }
+// else {
+//     alert("wrong");
 
-var tall = prompt("Am I not 150cm tall?");
-if (tall.toLowerCase() == "yes") {
-    alert("proudely yes!");
-    var correct = correct + 1;
-}
-else {
-    alert("2- wrong");
-}
+// }
 
-var sleep = prompt("Do I have control on my sleep scedule?");
-if (sleep.toLowerCase() == "no") {
-    alert("sadely");
-    var correct = correct + 1;
-}
-else {
-    alert("3- wrong");
-}
+// var tall = prompt("Am I not 150cm tall?");
+// if (tall.toLowerCase() == "yes" || tall.toLowerCase() == "y") {
+//     alert("proudely yes!");
+//     var correct = correct + 1;
+// }
+// else {
+//     alert("wrong");
+// }
 
-var plants = prompt("Do I like plants?");
-if (plants.toLowerCase() == "no") {
-    alert("correct");
-    var correct = correct + 1;
-}
-else {
-    alert("4- wrong");
-}
+// var sleep = prompt("Do I have control on my sleep scedule?");
+// if (sleep.toLowerCase() == "no" || sleep.toLowerCase() == "n") {
+//     alert("sadely");
+//     var correct = correct + 1;
+// }
+// else {
+//     alert("wrong");
+// }
 
-var physics = prompt("Am I good at physics?");
-if (physics.toLowerCase() == "yes") {
-    alert("correct");
-    var correct = correct + 1;
-}
-else {
-    alert("5- wrong");
-}
+// var plants = prompt("Do I like plants?");
+// if (plants.toLowerCase() == "no" || plants.toLowerCase() == "n") {
+//     alert("correct");
+//     var correct = correct + 1;
+// }
+// else {
+//     alert("wrong");
+// }
+
+// var physics = prompt("Am I good at physics?");
+// if (physics.toLowerCase() == "yes" || physics.toLowerCase() == "y") {
+//     alert("correct");
+//     var correct = correct + 1;
+// }
+// else {
+//     alert("wrong");
+// }
 
 //lab03
 //Q8
 
-var numGuess = prompt("Guess the number I have in mind. 4 atp");
 
-var randumN = Math.round(Math.random() * 10)
+var randumN = Math.round(Math.random() * 5)
+for (var i = 0; i <= 3; i++) {
+    console.log(randumN)
+    var numGuess = prompt("Guess the number I have in mind (0-5). 4 atp");
 
-if (numGuess == randumN) {
-    alert("correct!");
-    var correct = correct + 1;
-}
-else {
-    for (var i = 1; i <= 4; i++) {
-
-        if (numGuess < randumN ) {
-            alert("too low");
-            prompt("Guess the number I have in mind. 4 atp");
-
-        }
-        else if (numGuess >randumN) {
-            alert("too high");
-            prompt("Guess the number I have in mind. 4 atp");
-        }
-
+    if (numGuess == randumN) {
+        alert("correct!");
+        var correct = correct + 1;
+        break;
     }
 
+    else if (randumN > numGuess) {
+        alert("too low");
+        prompt("Guess the number I have in mind (0-5). 4 atp");
+
+    }
+    else if (randumN < numGuess) {
+        alert("too high");
+        prompt("Guess the number I have in mind (0-5). 4 atp");
+    }
+    else {
+        alert("what?")
+        
+    }
 }
 
 document.write('<h3>' + "for Q6 you tried " + i + "times " + "the correct answer is " + randumN + '</h3>')
@@ -82,28 +84,30 @@ document.write('<h3>' + "for Q6 you tried " + i + "times " + "the correct answer
 
 //Q7
 
-var qBrotherName = prompt("what are my brother's names? 6 atp");
-var aBrotherName = ["ahmed", "khaled", "anas"];
 
-for (var a = 1; a <= 6; a++) {
-    if (qBrotherName == aBrotherName) {
-        alert("correct!");
-        var correct = correct + 1;
 
+for (var a = 0; a < 6; a++) {
+    var qBrotherName. = prompt("what are my brother's names? 6 atp");
+
+    for (var y = 0 ; y < qBrotherName.length ; y++){
+        var aBrotherName = ["ahmed", "khaled", "anas"];
+        if (qBrotherName[y] == aBrotherName) {
+            alert("correct!");
+            var correct = correct + 1;
+        }
     }
-    else {
-        prompt("what are my brother's names? 6 atp");
-    }
-
+            alert("wrong")
+            
+       
 }
-
-
 
 document.write('<h3>' + "for Q7 you tried " + a + "times " + "the answers are " + aBrotherName + '</h3>');
 document.write('<h4>' + "your total is " + correct + '</h4>')
+
 if (correct == 0) {
     if (confirm("non is correct, you want to try again?")) {
         alert("reload");
     }
 }
+
 alert("Bye" + name);
